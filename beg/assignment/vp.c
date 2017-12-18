73743edcda87379511d31e71258d00aa5e41d681
vishnupriyaravi22 <34645232+vishnupriyaravi22@users.noreply.github.com>
2017-12-18 09:22:35 +0000

    Delete vp.c

--- beg/assignment/vp.c
+++
@@ -1,1 +0,0 @@
-#include<stdio.h>int main(){char name[] = "Sachin Ramesh Tendulkar";char nick[] = "Little Master";int score = 18426;float avg = 44.83;int a = 463;printf("%s\n",name);printf("%s\n",nick);printf("%d\n",score);printf("%3.2f\n",avg);printf("%d\n",a);return 0;}
