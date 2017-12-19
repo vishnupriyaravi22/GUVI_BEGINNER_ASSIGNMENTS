15b99fbdfecc4a2b7a7219893c752065462ddba1
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2017-12-19 05:12:15 +0000

    Add files via upload

---
+++ beg/assignment/vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+char s;
+
+scanf(" %s",&s);
+{
+if(s=='a'||s=='A'||s=='e'||s=='E'||s=='i'||s=='I'||s=='o'||s=='O'||s=='u'||s=='U')
+printf(" vowels");
+else 
+printf(" consonant");
+}
+
+return 0;
+}
+
