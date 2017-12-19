1e596ab30fa620481817a60c814e5dfb84f5eb42
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2017-12-19 05:24:16 +0000

    Add files via upload

---
+++ beg/assignment/largest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+
+printf("enter the numbers:");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+printf("a is largest");
+}
+else if(b>c)
+{
+printf("b is largest");
+}
+else
+{
+printf("c is largest");
+}
+return 0;
+}
